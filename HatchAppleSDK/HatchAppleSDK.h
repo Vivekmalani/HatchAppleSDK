4104ce85310912069cca6f7a16696c1027e7d549
Vivekmalani <vivek3693@gmail.com>
2022-06-26 10:36:54 +0530

    first commit

---
+++ HatchAppleSDK/HatchAppleSDK.h
@@ -0,0 +1,18 @@
+//
+//  HatchAppleSDK.h
+//  HatchAppleSDK
+//
+//  Created by MAC on 26/06/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HatchAppleSDK.
+FOUNDATION_EXPORT double HatchAppleSDKVersionNumber;
+
+//! Project version string for HatchAppleSDK.
+FOUNDATION_EXPORT const unsigned char HatchAppleSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HatchAppleSDK/PublicHeader.h>
+
+
