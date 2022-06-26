0dd9bd2aa758d8d9d846a7442a30dae4e153bd69
Vivekmalani <vivek3693@gmail.com>
2022-06-26 10:45:56 +0530

    Added SwiftyLib.podspec

---
+++ HatchAppleSDK/HatchApple.swift
@@ -0,0 +1,20 @@
+//
+//  HatchApple.swift
+//  HatchAppleSDK
+//
+//  Created by MAC on 26/06/22.
+//
+
+public final class HatchApple {
+
+    let name = "HatchApple"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
